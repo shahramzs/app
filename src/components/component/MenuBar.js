e74a3320b2e6e69932af5fba3e49144d7209a2d5
shahramzs <shahram.zarie.sarabi@gmail.com>
2025-11-11 19:12:54 +0330

    update

--- src/components/component/MenuBar.js
+++ src/components/component/MenuBar.js
@@ -14,14 +14,48 @@ import {
   MenubarSubTrigger,
   MenubarTrigger,
 } from "../ui/menubar";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogFooter,
+  DialogClose,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Settings, Search, User2, Download } from "lucide-react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
+import { useState } from "react";
 
 export function MenuBar() {
   const { theme, setTheme } = useTheme();
+  const [openMain, setOpenMain] = useState(false);
+  const [openLogin, setOpenLogin] = useState(false);
+  const [openPassword, setOpenPassword] = useState(false);
+
+  const handleGoToLogin = () => {
+    setOpenMain(false);
+    setOpenPassword(false);
+    setTimeout(() => setOpenLogin(true), 150);
+  };
+
+  const handleGoToMain = () => {
+    setOpenLogin(false);
+    setOpenPassword(false);
+    setTimeout(() => setOpenMain(true), 150);
+  };
+
+  const handleGoToPassword = () => {
+    setOpenMain(false);
+    setOpenLogin(false);
+    setTimeout(() => setOpenPassword(true), 150);
+  };
+
   return (
     <Menubar className="w-full flex justify-between h-[50px]">
       <div className="flex items-center gap-2">
@@ -58,14 +92,68 @@ export function MenuBar() {
             <Download className="cursor-pointer" />
           </Button>
         </MenubarMenu>
-
         <MenubarMenu>
-          <Button variant="outline" className="cursor-pointer">
-            <User2 />
-            <p className="text-sm text-gray-500">ورود به اکران</p>
-          </Button>
+          <Dialog open={openMain} onOpenChange={setOpenMain}>
+            <DialogTrigger>
+              <Button variant="outline" className="cursor-pointer">
+                <User2 />
+                <p className="text-sm text-gray-500">ورود به اکران</p>
+              </Button>
+            </DialogTrigger>
+            <DialogContent
+              className="sm:max-w-[425px]"
+              onInteractOutside={(e) => e.preventDefault()}
+              onEscapeKeyDown={(e) => e.preventDefault()}
+            >
+              <DialogHeader>
+                <DialogTitle className="self-center text-xl">
+                  ورود یا ثبت نام در اکران
+                </DialogTitle>
+                <DialogDescription className="self-center">
+                  برای ورود یا ثبت‌نام، اطلاعات کاربری خود را وارد کنید
+                </DialogDescription>
+              </DialogHeader>
+              <div className="grid gap-4">
+                <div className="grid gap-3">
+                  <Label htmlFor="name-1">ایمیل</Label>
+                  <Input
+                    id="email"
+                    name="پست الکترونیکی"
+                    placeholder="john.doe@gmail.com"
+                  />
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="username-1">شماره موبایل</Label>
+                  <Input id="mobile" name="mobile" placeholder="09125910037" />
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="username-1">کلمه عبور</Label>
+                  <Input id="password" name="password" placeholder="********" />
+                </div>
+                <div className="grid gap-3">
+                  <div className="flex flex-row justify-between gap-5">
+                    <Button
+                      className="w-[25%]"
+                      variant="Ghost"
+                      onClick={handleGoToLogin}
+                    >
+                      ورود به اکران
+                    </Button>
+                    <Button variant="Ghost" onClick={handleGoToPassword}>
+                      فراموشی رمز ورود
+                    </Button>
+                  </div>
+                </div>
+              </div>
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button variant="outline">لغو</Button>
+                </DialogClose>
+                <Button type="submit">ثبت نام</Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </MenubarMenu>
-
         <MenubarMenu>
           <MenubarTrigger className="mr-auto">
             <Settings />
@@ -107,6 +195,96 @@ export function MenuBar() {
           </MenubarContent>
         </MenubarMenu>
       </div>
+      <Dialog open={openLogin} onOpenChange={setOpenLogin}>
+        <DialogContent
+          className="sm:max-w-[400px]"
+          onInteractOutside={(e) => e.preventDefault()}
+          onEscapeKeyDown={(e) => e.preventDefault()}
+        >
+          <DialogHeader>
+            <DialogTitle className="self-center text-xl">
+              ورود به حساب کاربری
+            </DialogTitle>
+            <DialogDescription className="self-center">
+              لطفاً ایمیل و رمز عبور خود را وارد کنید
+            </DialogDescription>
+          </DialogHeader>
+
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="login-email">ایمیل</Label>
+              <Input
+                id="login-email"
+                type="email"
+                placeholder="you@email.com"
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="login-password">کلمه عبور</Label>
+              <Input
+                id="login-password"
+                type="password"
+                placeholder="********"
+              />
+            </div>
+            <Button>ورود</Button>
+            <div className="flex flex-row justify-between gap-5">
+              <Button variant="Ghost" onClick={handleGoToMain}>
+                ثبت نام در اکران
+              </Button>
+              <Button variant="Ghost" onClick={handleGoToPassword}>
+                فراموشی رمز ورود
+              </Button>
+            </div>
+          </div>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">بازگشت</Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+      <Dialog open={openPassword} onOpenChange={setOpenPassword}>
+        <DialogContent
+          className="sm:max-w-[400px]"
+          onInteractOutside={(e) => e.preventDefault()}
+          onEscapeKeyDown={(e) => e.preventDefault()}
+        >
+          <DialogHeader>
+            <DialogTitle className="self-center text-xl">
+              بازیابی کلمه عبور
+            </DialogTitle>
+            <DialogDescription className="self-center ">
+              لطفاً ایمیل خود را وارد کنید
+            </DialogDescription>
+          </DialogHeader>
+
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="login-email">ایمیل</Label>
+              <Input
+                id="login-email"
+                type="email"
+                placeholder="you@email.com"
+              />
+            </div>
+            <Button>ارسال</Button>
+            <div className="flex flex-row justify-between gap-5">
+              <Button variant="Ghost" onClick={handleGoToMain}>
+                ثبت نام در اکران
+              </Button>
+              <Button variant="Ghost" onClick={handleGoToLogin}>
+                ورود به اکران
+              </Button>
+            </div>
+          </div>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">بازگشت</Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </Menubar>
   );
 }
