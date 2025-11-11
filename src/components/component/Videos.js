0a1ed5e63ce6affc51e5dde7436cda34e7a3e1e7
shahramzs <shahram.zarie.sarabi@gmail.com>
2025-11-11 15:44:29 +0330

    update

--- src/components/component/Videos.js
+++ src/components/component/Videos.js
@@ -1,15 +1,9 @@
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+"use client";
+
 import { videos } from "@/utils/Constants";
 import Image from "next/image";
-
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Check } from "lucide-react";
 export default function Videos() {
   return (
     <div
@@ -20,19 +14,44 @@ export default function Videos() {
   "
     >
       {videos.map((video, i) => (
-        <Card key={i} className="w-full max-w-full h-50">
-          <CardContent>
+        <div key={i} className="w-full max-w-full overflow-hidden ">
+          <div className="p-0 relative w-full h-50">
             <Image
               src={video.image}
               alt={video.title}
-              width={100}
-              height={100}
+              fill
+              className="object-cover w-full h-full rounded-xl"
+              priority={false}
+              unoptimized
             />
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
+            <div className="absolute bottom-0 left-0 text-white bg-gray-700 rounded-md p-1 items-center justify-center text-xs mb-2 ml-2">
+              {video.duration}
+            </div>
+          </div>
+
+          <div className="p-2">
+            <p className="text-sm font-medium">{video.title}</p>
+            <div className="flex flex-row gap-3 mt-2 items-center">
+              <Avatar>
+                <AvatarImage src={video.avatar} />
+                <AvatarFallback>{video.channel}</AvatarFallback>
+              </Avatar>
+              <p className="text-sm font-small">{video.channel}</p>
+              <Check
+                size={18}
+                color={"green"}
+                strokeWidth={8}
+                className="border bg-gray-200 rounded-xl p-1"
+              />
+            </div>
+            <div className="flex flex-row gap-2 mt-1">
+              <p className="text-xs font-small text-gray-500">
+                {video.view} بازدید
+              </p>{" "}
+              -<p className="text-xs font-small text-gray-500">{video.time}</p>
+            </div>
+          </div>
+        </div>
       ))}
     </div>
   );
